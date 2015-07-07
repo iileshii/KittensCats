73d0229938ee07030f864c8bb9bb9926f2935860
iileshii <kuznetsov.a.e@gmail.com>
2015-07-07 17:30:42 +0600

    Toolbar style changed Russian added

--- app/src/main/java/com/example/android/kittenscats/MainActivity.java
+++ app/src/main/java/com/example/android/kittenscats/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.kittenscats;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,67 +23,71 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(isOnline()) {
+            setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+            RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        final PicassoRecyclerAdapter picassoRecyclerAdapter =
-                new PicassoRecyclerAdapter(this, mPhotoList);
-        FetchJsonPhotoList fetchJsonPhotoList = new FetchJsonPhotoList(this,
-                mPhotoList,
-                picassoRecyclerAdapter);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+            final PicassoRecyclerAdapter picassoRecyclerAdapter =
+                    new PicassoRecyclerAdapter(this, mPhotoList);
+            FetchJsonPhotoList fetchJsonPhotoList = new FetchJsonPhotoList(this,
+                    mPhotoList,
+                    picassoRecyclerAdapter);
 
-        RecyclerView.OnScrollListener scrollListener =
-                new EndlessRecyclerOnScrollListener(layoutManager) {
-                    @Override
-                    public void onLoadMore(int current_page) {
-                        FetchJsonPhotoList mFetchJsonPhotoList =
-                                new FetchJsonPhotoList(getBaseContext(),
-                                        mPhotoList,
-                                        picassoRecyclerAdapter);
-                        FetchJsonPhotoList.addPageNumber();
-                        mFetchJsonPhotoList.execute();
+            RecyclerView.OnScrollListener scrollListener =
+                    new EndlessRecyclerOnScrollListener(layoutManager) {
+                        @Override
+                        public void onLoadMore(int current_page) {
+                            FetchJsonPhotoList mFetchJsonPhotoList =
+                                    new FetchJsonPhotoList(getBaseContext(),
+                                            mPhotoList,
+                                            picassoRecyclerAdapter);
+                            FetchJsonPhotoList.addPageNumber();
+                            mFetchJsonPhotoList.execute();
 
-                    }
-                };
+                        }
+                    };
 
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.scrollToPosition(0);
-        recyclerView.setAdapter(picassoRecyclerAdapter);
-        recyclerView.addOnScrollListener(scrollListener);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.scrollToPosition(0);
+            recyclerView.setAdapter(picassoRecyclerAdapter);
+            recyclerView.addOnScrollListener(scrollListener);
 
-        RecyclerItemClickListener.OnItemClickListener onItemClickListener =
-                new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                        Photo currentImage = mPhotoList.get(position);
-                        Intent intent = new Intent(getBaseContext(), PhotoDetailsActivity.class);
-                        intent.putExtra("PHOTO_TRANSFER", currentImage);
-                        startActivity(intent);
-                    }
+            RecyclerItemClickListener.OnItemClickListener onItemClickListener =
+                    new RecyclerItemClickListener.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(View view, int position) {
+                            Photo currentImage = mPhotoList.get(position);
+                            Intent intent = new Intent(getBaseContext(), PhotoDetailsActivity.class);
+                            intent.putExtra("PHOTO_TRANSFER", currentImage);
+                            startActivity(intent);
+                        }
 
-                    @Override
-                    public void onItemLongClick(View view, int position) {
-                        Photo currentImage = mPhotoList.get(position);
+                        @Override
+                        public void onItemLongClick(View view, int position) {
+                            Photo currentImage = mPhotoList.get(position);
 
-                        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                        shareIntent.setType("text/plain");
-                        shareIntent.putExtra(Intent.EXTRA_TEXT,
-                                "Photo: " + currentImage.toShortUrl() +
-                                        "\n" + "by: " + currentImage.getExistName() +
-                                        "\n" + "Kittens & Cats app");
-                        Intent.createChooser(shareIntent, "Share catlink to..");
-                        startActivity(shareIntent);
-                    }
-                };
+                            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                            shareIntent.setType("text/plain");
+                            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                                    "Photo: " + currentImage.toShortUrl() +
+                                            "\n" + "by: " + currentImage.getExistName() +
+                                            "\n" + "Kittens & Cats app");
+                            Intent.createChooser(shareIntent, "Share catlink to..");
+                            startActivity(shareIntent);
+                        }
+                    };
 
-        RecyclerItemClickListener recyclerItemClickListener =
-                new RecyclerItemClickListener(this, recyclerView, onItemClickListener);
+            RecyclerItemClickListener recyclerItemClickListener =
+                    new RecyclerItemClickListener(this, recyclerView, onItemClickListener);
 
-        recyclerView.addOnItemTouchListener(recyclerItemClickListener);
+            recyclerView.addOnItemTouchListener(recyclerItemClickListener);
 
-        fetchJsonPhotoList.execute();
+            fetchJsonPhotoList.execute();
+        } else {
+            setContentView(R.layout.activity_main_offline);
+        }
 
     }
 
@@ -100,18 +106,18 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-//            Picasso.with(this).load(mFetchCatsImage.getUrlString(0)).into(imageView);
-//            Picasso.with(this).load(mFetchCatsImage.getUrlString(1)).into(imageView2);
-            Toast.makeText(this, "Hi! How are you? Settings", Toast.LENGTH_SHORT).show();
+        if (id == R.id.action) {
+            Toast.makeText(this, "Hello! :)", Toast.LENGTH_SHORT).show();
             return true;
         }
-
-        if (id == R.id.action_fill) {
-            Toast.makeText(this, "Hi! How are you? Fill", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null &&
+                cm.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 }
